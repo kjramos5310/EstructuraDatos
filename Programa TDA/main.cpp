1e7b2691d10d1129a9257b899480cb0da9f29d20
kjramos5310 <kjramos@espe.edu.ec>
2022-05-11 07:53:02 -0500

    v0.2

--- Programa TDA/main.cpp
+++ Programa TDA/main.cpp
@@ -1,3 +1,15 @@
+/*********************************************************************************************
+*UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE													 *
+*Programa para implementar: TDA																 *
+*Autor(es): Victor Camacho, Kevin Ramos														 *
+*Fecha de creacion: 09/05/2022																 *
+*Fecha de modificacion: 10/05/2022															 *
+*Grupo #																					 *
+*Github del grupo: https://github.com/kjramos5310/EstructuraDatos/tree/master/Programa%20TDA *
+*Version: 0.1																				 *
+*ESTRUCTURA DE DATOS 4683																	 *
+*																							 *
+**********************************************************************************************/
 #include <iostream>
 #include "Ingresar.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -10,3 +22,5 @@ int main(int argc, char** argv) {
 	cout<< dato1->ingresarDato(dato);
 	return 0;
 	}
+	
+	
