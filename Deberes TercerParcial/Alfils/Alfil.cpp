48e3a82373dab689117d01cdebe05d4b90c51f9b
kjramos5310 <105337663+kjramos5310@users.noreply.github.com>
2022-08-04 23:25:05 -0500

    Deberes U3

---
+++ Deberes TercerParcial/Alfils/Alfil.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+class Alfil{
+	public:
+		bool **validar;
+		char **tablero;
+		fstream archivo;
+		int contador;
+		int n=8;
+		int numR;
+		void encerarTablero(){
+			for(int i=0;i<n;i++){
+				validar[i]=new bool[n];
+				tablero[i]=new char[n];
+				for(int j=0;j<n;j++){
+					*(*(validar+i)+j)=false;
+					*(*(tablero+i)+j)='*';
+				}
+			}
+		}
+	void crearArchivo(){
+		archivo.open("exaustivoAlfil.txt",fstream::out);
+		archivo<<"solucion"<<n<<"*"<<n<<endl<<endl;
+		contador=0;
+	}
+	Alfil(int n1){
+		numR=n1;
+		validar=new bool *[n];
+		tablero=new char *[n];
+		encerarTablero();
+		crearArchivo();
+	}
+	void bloquear(int x, int y){
+		int aux1,aux2;
+		aux2=y;
+		aux1=0;
+		while(aux1<n){//vertical
+			*(*(validar+aux1)+aux2)=false;
+			aux1++;
+		}
+		aux2=0;
+		aux1=x;
+		while(aux2<n){
+			*(*(validar+aux1)+aux2)=false;
+			aux2++;
+		}
+		aux2=y;//diagonal
+		aux1=x;
+		while(aux1>0&&aux2>0){
+			aux1--;
+			aux2--;
+		}
+		aux2=y;
+		aux1=x;
+		while(aux1<n && aux2>0){
+			aux1++;
+			aux2--;
+		}
+		aux1--;
+		aux2++;
+		while(aux1>=0 && aux2<n){
+			*(*(validar+aux1)+aux2)=true;
+			aux1--;
+			aux2++;
+		}
+	}
+	
+	void mostrar(){
+		contador++;
+		archivo<<"solucion N "<<contador <<endl;
+		//cout<<"solucion N "<<contador<<endl;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				archivo<<*(*(tablero+i)+j)<<" ";
+				//cout<<*(*(tablero+i)+j)<<" ";
+			}
+			archivo<<endl;
+			//cout<<endl;
+		}
+		archivo<<endl;
+		//cout<<endl;
+	}
+	void mostrarCMD(){
+		//contador1++;
+		cout<<"solucion N "<<contador <<endl;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				cout<<*(*(tablero+i)+j)<<" ";
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+	
+	void quitarRelleno(int x, int y){
+		*(*(tablero +x)+y)='*';
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				*(*(validar +i)+j)=false;
+			}
+		}
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				if(*(*(tablero +i)+j)=='R')
+				bloquear(i,j);
+			}	
+		}
+	}
+
+	
+	void solucion(int x, int y, int n1){
+		*(*(tablero +x)+y)='A';
+		bloquear(x,y);
+		if(n1==numR){
+			mostrar();
+			mostrarCMD();
+		}else{
+			for(int i=0;i<n;i++){
+				if(*(*(validar +i )+ y+1)==false){
+					solucion(i,y+1,n1+1);
+				}
+			}
+		}
+		quitarRelleno(x,y);
+	}
+	void solucionAlfils(){
+		for(int i=0;i<n;i++){
+			solucion(i,0,1);
+		}
+		archivo.close();	
+	}
+};
+int main(int argc, char** argv) {
+	int n;
+	cout<<"\tIngrese el numero de Alfils"<<endl;
+	cin>>n;
+	Alfil *obj=new Alfil(n);
+	obj->solucionAlfils();
+	cout<<"Se creo el archivo txt"<<endl;
+	return 0;
+}
